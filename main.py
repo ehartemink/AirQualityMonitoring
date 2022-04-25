7c44cfe0e4230759c91ceb7ccce780f67b6b72a4
ehartemink <edwardhartemink@gmail.com>
2022-04-24 23:05:24 -0500

    added data cleaning daemon for old data

--- main.py
+++ main.py
@@ -35,12 +35,16 @@ def read_and_print_data(wakeword, sensor, mediator, db):
 		raw_data = sensor.read_data()
 		processed_data = mediator.transform(raw_data, wakeword)
 		db.batch_write(processed_data)
-		
+def data_cleaning_daemon(db):
+	while True:
+		time.sleep(60)		
+		db.remove_old_logs()
 
 new_threads = {} 
 
 for (port, sensor) in sensors.items():
 	new_threads[port] = threading.Thread(target=read_and_print_data, name=port, args=(port, sensor, mediator, db))
+new_threads["remove_old_logs"] = threading.Thread(target=data_cleaning_daemon, name="remove old logs", args=[db])
 
 for port, thread in new_threads.items():
 	thread.start()
