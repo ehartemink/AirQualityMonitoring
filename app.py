58de857afa5a19ddcfe8ab39a74144f158e00810
ehartemink <edwardhartemink@gmail.com>
2022-04-24 23:04:18 -0500

    implemented single figure with subfigures

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import database
 
 app = Dash(__name__)
 
+INITIAL_LOOKBACK_WINDOW = 2
+
 database_path = "persist/table.csv"
 lookbacks = ["1 min", "5 min", "1 hour", "24 hours", "1 week"]
 lookbacks_in_seconds = {
@@ -20,44 +22,50 @@ lookbacks_in_seconds = {
 	"24 hours": 86400,
 	"1 week": 604800
 }
-db = database.DataBase(database_path)
-df = db.open_table()
-df_by_metric = {metric: df for (metric, df) in df.groupby("metric")}
 
-def create_figure(metric, df, lookback):
-	seconds_lookback=lookbacks_in_seconds[lookback]
-	df = df[df.timestamp > (dt.datetime.now() - dt.timedelta(seconds=seconds_lookback)).timestamp()]
-	fig = go.Figure(data=go.Scattergl(
-    	x=df["timestamp"],
-    	y=df["value"],
-    	mode='markers'
-	))
-
-	fig.update_layout(
-    	title=metric.upper(),
-    	xaxis_title="Time",
-    	yaxis_title="Value",
-    	legend_title="Legend Title",
-    	font=dict(
-        	family="Garamond",
-        	size=8,
-        	color="#1E90FF"
-    	)
-	)
+def create_figure(df, lookback):
+	df_by_metric = {metric: df for (metric, df) in df.groupby("metric")}
+	metrics = sorted(df_by_metric.keys())
+	total_subplots = len(metrics)
+	fig = make_subplots(rows=(total_subplots+1)//2, cols=2, subplot_titles=metrics)
+	for (i, metric) in enumerate(metrics):
+		df = df_by_metric[metric]
+		row_idx = (i // 2) + 1
+		col_idx = (i % 2) + 1
+		try:
+			fig.add_scattergl(
+			x=df["timestamp"],
+			y=df["value"],
+			row=row_idx,
+			col=col_idx,
+			mode="markers",
+			marker={"size": 5}
+			)
+		except Exception as e:
+			print("Exception: ", e)
+			print(df)
+		fig.update_xaxes(title_text="Timestamp", row=row_idx, col=col_idx)
+		fig.update_yaxes(title_text=metric, row=row_idx, col=col_idx)
+	fig.update_layout(height=3000, width=2000)
 	return fig
 
-def refresh_figures_cache(metrics, db, figures_cache):
-	for metric in metrics:
-		df = db.open_table()
-		df_by_metric = {metric: df for (metric, df) in df.groupby("metric")}
-		df = df_by_metric[metric]
-		for lookback in lookbacks:
-			fig = create_figure(metric, df, lookback)
-			figures_cache[metric + lookback] = fig
+def refresh_figures_cache(db, figures_cache):
+	for lookback_window in range(0, 5):
+		try:
+			start = dt.datetime.now()
+			df = db.open_table()
+			seconds_lookback=lookbacks_in_seconds[lookbacks[lookback_window]]
+			df = df[df.timestamp > (dt.datetime.now() - dt.timedelta(seconds=seconds_lookback)).timestamp()]
+			df.timestamp = [dt.datetime.fromtimestamp(val).strftime("%Y-%m-%d %H:%M:%S") for val in df["timestamp"]]
+			fig = create_figure(df, lookback_window)
+			figures_cache[lookback_window] = fig
+		except:
+			print("Write figures failed for ", lookbacks[lookback_window])
+		print("Write figures cache for ", lookbacks[lookback_window], dt.datetime.now() - start)
 
-def figures_cache_daemon(metrics, db, figures_cache):
+def figures_cache_loop(db, figures_cache):
 	while True:
-		refresh_figures_cache(metrics, db, figures_cache)
+		refresh_figures_cache(db, figures_cache)
 
 def write_graph_id(prefix):
 	return f"{prefix}_graph".replace(".", "")
@@ -70,7 +78,7 @@ def create_graph_div(metric):
 		html.H2(metric),
 		dcc.Graph(
 			id=write_graph_id(metric),
-			figure=figures_by_metric_lookback[metric + "1 hour"],
+			figure=figures_cache[INITIAL_LOOKBACK_WINDOW],
 			config={
 				'displayModeBar': False
 			}
@@ -79,21 +87,23 @@ def create_graph_div(metric):
 	id=write_div_id(metric),
 	style={'width': '49%', 'display': 'inline-block'})
 	
+db = database.DataBase(database_path)
+df = db.open_table()
+df_by_metric = {metric: df for (metric, df) in df.groupby("metric")}
+figures_cache = database.ThreadSafeDict()
+refresh_figures_cache(db, figures_cache)
+figures_cache_daemon = Thread(target=figures_cache_loop, name="figures-cache-daemon", args=(db, figures_cache))
+figures_cache_daemon.start()
 
 app.layout = html.Div(children=[
     html.H1(children='Welcome to my dashboard'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
 
 	html.Label('Lookback Window'),
 		dcc.Slider(
             min=0,
             max=4,
 			marks=dict(enumerate(lookbacks)),
-            value=2,
+            value=INITIAL_LOOKBACK_WINDOW,
 			step=1,
 			included=False,
 			id='lookback-window'
@@ -109,7 +119,7 @@ app.layout = html.Div(children=[
 
 	dcc.Interval(
             id='interval-component',
-            interval=15*1000, # in milliseconds
+            interval=750, # in milliseconds
             n_intervals=0)
 ])
 
@@ -121,32 +131,7 @@ app.layout = html.Div(children=[
 				Input("interval-component", "n_intervals")
 		)
 def create_figures(lookback_window, n):
-	df = db.open_table()
-	df_by_metric = {metric: df for (metric, df) in df.groupby("metric")}
-	metrics = sorted(df_by_metric.keys())
-	total_subplots = len(metrics)
-	fig = make_subplots(rows=(total_subplots+1)//2, cols=2, subplot_titles=metrics)
-	for (i, metric) in enumerate(metrics):
-		seconds_lookback=lookbacks_in_seconds[lookbacks[lookback_window]]
-		df = df_by_metric[metric]
-		print(df.shape)
-		df = df[df.timestamp > (dt.datetime.now() - dt.timedelta(seconds=seconds_lookback)).timestamp()]
-		print(df.shape)
-		row_idx = (i // 2) + 1
-		col_idx = (i % 2) + 1
-		df = df_by_metric[metric]
-		fig.add_scattergl(
-			x=df["timestamp"],
-			y=df["value"],
-			row=row_idx,
-			col=col_idx,
-			mode="markers",
-			marker={"size": 5}
-		)
-		fig.update_xaxes(title_text="Timestamp", row=row_idx, col=col_idx)
-		fig.update_yaxes(title_text=metric, row=row_idx, col=col_idx)
-	fig.update_layout(height=3000, width=2000)
-	return fig
+	return figures_cache[lookback_window]
 	
 
 if __name__ == '__main__':
