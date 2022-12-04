94819dc8c0a6e765f2ae2841bb8f46e8d746faea
ehartemink <edwardhartemink@gmail.com>
2022-12-03 18:09:20 -0800

    Changing deployment script to better name GNU screens

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ sudo mkdir /run/screen
 sudo chmod 777 /run/screen
 screen -X -S main kill
 screen -X -S app kill
-screen -d -S main -m python3 main.py -S
-screen -d -S app -m python3 app.py -S
+screen -d -S air_quality_data_ingestion -m python3 main.py
+screen -d -S air_quality_dashboard -m python3 app.py
